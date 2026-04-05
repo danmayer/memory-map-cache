172075488f8eaae8f9ea5d83a206d3c6d4b092ed
Dan Mayer <dan@mayerdan.com>
2026-04-04 20:16:29 -0600

    Deploy: Force explicit compilation of native C-extensions explicitly securely during the Render physical container initialization since gem pathing structurally bypasses automated compilation

--- demo/bin/render-build.sh
+++ demo/bin/render-build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+echo "Compiling Native C-Extensions for memory-map-cache..."
+cd ..
+bundle install
+bundle exec rake compile
+cd demo
+
+echo "Building Rails Environment..."
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
