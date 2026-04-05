0022e88304f817c598fda4e3f87a38ec8c529000
Dan Mayer <dan@mayerdan.com>
2026-04-04 20:25:09 -0600

    Deploy: Bypass strict Render Ubuntu Bundler deployment lockfile crashes securely dynamically compiling the raw C-extension exclusively through Make instead

--- demo/bin/render-build.sh
+++ demo/bin/render-build.sh
@@ -2,11 +2,12 @@
 # exit on error
 set -o errexit
 
-echo "Compiling Native C-Extensions for memory-map-cache..."
-cd ..
-bundle install
-bundle exec rake compile
-cd demo
+echo "Compiling Native C-Extensions manually for memory-map-cache..."
+cd ../ext/memory_map_cache
+ruby extconf.rb
+make
+cp memory_map_cache.so ../../lib/
+cd ../../demo
 
 echo "Building Rails Environment..."
 bundle install
